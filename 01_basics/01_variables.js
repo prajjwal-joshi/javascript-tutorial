0e2b8edc4c24f4e56be9bb594c04f4a52378ed55
Prajjwal Joshi <122428222+prajjwal-joshi@users.noreply.github.com>
2024-07-15 05:52:51 +0000

    data types

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,15 @@
+const accountId = 144553
+let accountEmail = "prajjwaljoshi2197@gmail.com"
+var accountPassword = "12345" //var is not used due to issue in block scope and functional scope
+let accountState;
+accountCity = "Delhi" //not preferred
+
+
+// accountId = 2
+accountEmail = "hc@hc.com"
+accountPassword = "21122112"
+accountCity = "Bengaluru"
+
+console.log(accountId);
+
+console.table([accountId, accountEmail, accountPassword, accountCity, accountState]);
---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,20 @@
+"use strict"; // treat all JS code as newer version
+
+// alert( 3 + 3)// cannot use directly as we are using node not browse
+
+let name = "prajjwal"
+let age = 20
+let isLoggedIn = false
+let state = null
+
+//number => 2 to power 53
+//bigint
+//string => ""
+//boolean => true/false
+//null => standalone value
+//undefined => value not assigned
+//symbol => unique
+
+//object
+
+console.log(typeof undefined);
