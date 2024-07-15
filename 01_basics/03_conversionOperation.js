f39c53948226a065207d48a646f74fe803c50103
Prajjwal Joshi <122428222+prajjwal-joshi@users.noreply.github.com>
2024-07-15 06:03:57 +0000

    conversion of datatypes

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,27 @@
+let score = "prajjwal"
+
+// console.log(typeof score);
+// console.log(typeof(score));
+
+
+let valueInNumber = Number(score)
+
+// console.log(valueInNumber);
+
+//"33" =>33
+//"33abc" => NaN
+// true => 1; false => 0
+
+let isLoggedIn = "0"
+
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+
+// console.log(booleanIsLoggedIn);
+
+//1 =>true; 0=> false
+//""=>false;
+//"prajjwal" =>true
+
+let someNumber = 33
+let stringNumber= String(someNumber)
+console.log(typeof stringNumber);
